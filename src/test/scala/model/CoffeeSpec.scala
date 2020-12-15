c8d25b1618081379e7c0f44350d8b3d8151bd8d1
ryota0624 <tennis.suta6@gmail.com>
2020-12-15 16:38:45 +0900

    add boring test

--- src/test/scala/model/CoffeeSpec.scala
+++ src/test/scala/model/CoffeeSpec.scala
@@ -1,6 +1,34 @@
 package model
 
+import org.scalatest.matchers.should.Matchers._
 import org.scalatest.wordspec.AnyWordSpecLike
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-class CoffeeSpec extends AnyWordSpecLike with ScalaCheckDrivenPropertyChecks {}
+class CoffeeSpec extends AnyWordSpecLike with ScalaCheckDrivenPropertyChecks {
+  "constructor" should {
+    "throw error when received 0" in {
+      an[IllegalArgumentException] should be thrownBy {
+        new Coffee(0)
+      }
+    }
+
+    "throw error when received negative number" in {
+      an[IllegalArgumentException] should be thrownBy {
+        new Coffee(-1)
+      }
+    }
+
+    "not throw error when receive positive number" in {
+      noException should be thrownBy {
+        new Coffee(1)
+      }
+    }
+  }
+
+  "amount" should {
+    "be as same as constructor passed amount" in {
+      val amount = 1
+      new Coffee(amount).amount shouldBe amount
+    }
+  }
+}
