08c00442e05919838c7cce8cc69548eab7f4975c
ryota0624 <tennis.suta6@gmail.com>
2020-12-15 16:17:26 +0900

    add require

--- src/main/scala/model/Coffee.scala
+++ src/main/scala/model/Coffee.scala
@@ -1,6 +1,8 @@
 package model
 
 class Coffee(val amount: Int) {
+  require(amount > 0, "amount should be greeter 0")
+
   // コーヒーを混ぜ合わせる
   def mix(other: Coffee): Coffee = new Coffee(amount = amount + other.amount)
 }
