5c435e95a6392aeb7e65a85dd8d096bdec9f9293
ryota0624 <tennis.suta6@gmail.com>
2021-02-19 15:31:15 +0900

    :sparkles: add dynamodb impl

--- build.sbt
+++ build.sbt
@@ -4,8 +4,14 @@ version := "0.1"
 
 scalaVersion := "2.13.4"
 
+
+
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.2.2" % Test,
   "org.scalatestplus" %% "scalacheck-1-14" % "3.2.2.0" % Test,
   "com.dimafeng" %% "testcontainers-scala-scalatest" % "0.39.1" % Test
+)
+
+libraryDependencies ++= Seq(
+  "software.amazon.awssdk"  % "dynamodb"           % "2.13.17",
 )
--- src/main/scala/model/Message.scala
+++ src/main/scala/model/Message.scala
@@ -1,6 +1,12 @@
 package model
 
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model._
+
+import java.net.URI
 import java.util.UUID
+import scala.jdk.CollectionConverters.{MapHasAsJava, MapHasAsScala}
 
 case class Message(id: String, text: String)
 object Message {
@@ -11,4 +17,85 @@ object Message {
 trait Messages {
   def add(message: Message)
   def get(id: String): Option[Message]
+}
+
+object MessagesOnDymamodb {
+  private val tableName = "messages"
+
+  def createTable(messages: MessagesOnDymamodb): Unit = {
+
+    messages.client.createTable(
+      CreateTableRequest
+        .builder()
+        .tableName(tableName)
+        .attributeDefinitions(
+          AttributeDefinition.builder
+            .attributeName("id")
+            .attributeType(ScalarAttributeType.S)
+            .build()
+        )
+        .keySchema(
+          KeySchemaElement.builder
+            .attributeName("id")
+            .keyType(KeyType.HASH)
+            .build()
+        )
+        .provisionedThroughput(
+          ProvisionedThroughput
+            .builder()
+            .readCapacityUnits(100)
+            .writeCapacityUnits(100)
+            .build()
+        )
+        .build()
+    )
+  }
+}
+
+case class MessagesOnDymamodb(endpoint: URI, region: Region) extends Messages {
+  import MessagesOnDymamodb._
+  private val client: DynamoDbClient = {
+    DynamoDbClient
+      .builder()
+      .endpointOverride(endpoint)
+      .region(region)
+      .build()
+  }
+
+  override def add(message: Message): Unit = {
+    val request = PutItemRequest
+      .builder()
+      .tableName(tableName)
+      .item(
+        Map(
+          "id" -> message.id,
+          "text" -> message.text
+        ).view
+          .mapValues(value => AttributeValue.builder().s(value).build())
+          .toMap
+          .asJava
+      )
+    client.putItem(request.build())
+  }
+
+  override def get(id: String): Option[Message] = {
+    val key = AttributeValue.builder().s(id).build()
+    val request =
+      GetItemRequest
+        .builder()
+        .tableName(tableName)
+        .key(Map("id" -> key).asJava)
+        .build()
+    val resultMap = client.getItem(request).item().asScala
+    if (resultMap.isEmpty) {
+      None
+    } else {
+      Some(
+        Message(
+          resultMap("id").s(),
+          resultMap("text").s()
+        )
+      )
+    }
+  }
 }
