8a4494e05117c4851a4d59e6fee27ab815d7d153
ryota0624 <tennis.suta6@gmail.com>
2021-02-19 15:58:36 +0900

    :sparkles: add test-containers to spec

--- src/test/scala/model/MessagesSpec.scala
+++ src/test/scala/model/MessagesSpec.scala
@@ -1,15 +1,46 @@
 package model
 
+import com.dimafeng.testcontainers.{DockerComposeContainer, ExposedService, ForAllTestContainer}
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.OptionValues._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpecLike
+import software.amazon.awssdk.regions.Region
 
-class MessagesSpec extends AnyWordSpecLike with Matchers {
-  val messages = new Messages {
-    override def add(message: Message): Unit = ???
+import java.io.File
+import java.net.URI
 
-    override def get(id: String): Option[Message] = ???
+class MessagesSpec
+    extends AnyWordSpecLike
+    with Matchers
+    with BeforeAndAfterAll
+    with ForAllTestContainer {
+  override val container: DockerComposeContainer =
+    new DockerComposeContainer(
+      composeFiles =
+        new File(getClass.getResource("/docker-compose.yml").getFile),
+      Seq(
+        ExposedService("localstack", 4566)
+      )
+    )
+
+  lazy val messages: Messages = {
+    val (localstackContainerHost, localstackContainerHostPort) =
+      (
+        container.getServiceHost("localstack", 4566),
+        container.getServicePort("localstack", 4566)
+      )
+
+    val messages = MessagesOnDymamodb(
+      new URI(s"http://${localstackContainerHost}:${localstackContainerHostPort}"),
+      Region.of("ap-northeast-1")
+    )
+
+    MessagesOnDymamodb.createTable(messages)
+
+    messages
   }
+
   "messages" should {
     "追加済のメッセージを取得できる" in {
       val message = Message("hello")
